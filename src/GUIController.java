8242ed62054deb820672d06d3ec35bc8ab0970bb
Sam Montoya <smon7958@740s-pl-118.local>
2016-01-19 13:55:27 -0700

    Finished up More

--- src/GUIController.java
+++ src/GUIController.java
@@ -6,16 +6,23 @@ public class GUIController extends JFrame
 		/** 	Declaration Section (Goes underneath the class name, and above the constructor.)
 		 * 	Every variable that will be used in more than one method needs to be declared here. Not initialized.
 		 */
-		private GridLayout baseLayout;
+		private SpringLayout baseLayout;
+		private JButton simpleButton;
 		
 		
 		/** 	Constructor (Goes directly under the declaration section)
 		 * 	Where variables that will be used throughout the class, or in more than one method, will be initialized. 
+		 * 	Also where you call other methods.
 		 * 	[Variables from the Declaration Section]
 		 */
 		public GUIController()
 		{
-			baseLayout = new GridLayout();
+			baseLayout = new SpringLayout();
+			simpleButton = new JButton("A Text for the Button");
+			
+			buildFrame();
+			buildPanel();
+			buildListeners();
 		}
 		
 		/**
@@ -34,5 +41,18 @@ public class GUIController extends JFrame
 			
 			//setVisible has to be the last line of this method.
 			setVisible(true);
+		}
+		
+		/** Puts Buttons, Text Fields, etc into the Program.
+		 * Where you add all the components into the Panel.
+		 */
+		private void buildPanel()
+		{
+			add(simpleButton);
+		}
+		
+		private void buildListeners()
+		{
+			
 		}
 	}
